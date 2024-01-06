be3596f44fe76a2b6e3b2279df7dedc336199683
Hariom <82692072+DevHarry066@users.noreply.github.com>
2024-01-06 10:27:06 -0800

    Added Navbar, Footer and Main with Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
+import About from "./components/pages/About";
 function App() {
   return (
-    <div className="App">
-      <h1 className="btn">Hello</h1>
-    </div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
